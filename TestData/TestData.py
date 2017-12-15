f393870729f0cc616c72fa7931c3d6fa9a544b38
virendra Ukey <viruukey@gmail.com>
2017-12-15 15:47:34 +0530

    update

--- TestData/TestData.py
+++ TestData/TestData.py
@@ -1,16 +1,21 @@
 import json
+
 class TestData(object):
     def __init__(self, location, fileName):
         self.location = location
         self.fileName = fileName
-     
-    
+   
+  
     def data(self):
         try:
             test_data = open(self.location+self.fileName, 'r')
         except Exception as e:
             print ("Issue with test data file", e)
         json_str = json.load(test_data)
+        test_data.close()
         return json_str
+    
+    
+            
     
 
