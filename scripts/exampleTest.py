be5f957b9cbf463a0fd7019e70c63f2c099fdff0
virendra Ukey <viruukey@gmail.com>
2017-12-17 17:39:05 +0530

    Adding example test case

--- scripts/exampleTest.py
+++ scripts/exampleTest.py
@@ -1,14 +1,19 @@
-import unittest
-from TestData.TestData import TestData
-import setupTest
-from api import Api
-import constructURL
-import re
+try:
+    import unittest
+    from TestData.TestData import TestData
+    import setupTest
+    from api import Api
+    import constructURL
+    import re
+    import logging
+except ImportError as e:
+    print ("Import of module in test failed", e)
 
+logging.basicConfig(level=logging.DEBUG)
 setuptest = setupTest.setupTest
 
 class ExampleTest(unittest.TestCase):
-        
+    
     @classmethod
     def setup_class(cls):
         try:
@@ -21,9 +26,10 @@ class ExampleTest(unittest.TestCase):
             print ("Exception occurred during setup", e)
     
     def testRun(self):
-        """Verify plain RestAPI GET request"""
+        log = logging.getLogger('Running example test for REST GET Request')
         try:
             json_str = self.setup_class()
+            log.debug("Constructing URL")
             if json_str['url1']['path']:
                 url = constructURL.constURL(baseurl=json_str['url1']['baseurl'], 
                                             path=json_str['url1']['path'])
@@ -32,14 +38,17 @@ class ExampleTest(unittest.TestCase):
                 
             req = Api(url=url)
             
+            log.debug("Check response of GET Request")
             resp = req.get()
             self.assertEqual(200, resp.status_code)
+            log.debug("Verifying response type is json")
             response_type = resp.headers["content-type"]
             if re.search(r'json', response_type):
                 response = resp.json()
         except Exception as e:
             print ("Exception occurred during test run", e)
-                
+        
+        log.debug("Finally verify test case expected output")        
         self.assertEqual("Afghanistan", response['RestResponse']['result'][0]['name'])
         
 
